0c97de76d0d636f3e471b72d044dc4c981918500
Matt <mattbdev@outlook.com>
2015-02-05 20:00:24 -0500

    change in message

--- src/main/java/com/worldcretornica/plotme_core/PlotMeCoreManager.java
+++ src/main/java/com/worldcretornica/plotme_core/PlotMeCoreManager.java
@@ -435,7 +435,7 @@ public class PlotMeCoreManager {
         try {
             firstWorld = (String) getPlotMaps().keySet().toArray()[0];
         } catch (ArrayIndexOutOfBoundsException error) {
-            plugin.getLogger().warning("Uh oh. You don't have any plotworlds so there is gonna be a bunch of errors now.");
+            plugin.getLogger().warning("Uh oh. You don't have any plotworlds so plotme isn't working properly.");
         }
         return plugin.getServerBridge().getWorld(firstWorld);
     }
