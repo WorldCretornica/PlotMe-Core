05a27771aed906f67d2b9661a8d88818815c14c2
BuildTools <unconfigured@null.spigotmc.org>
2015-03-05 01:15:46 -0500

    Readding removed method Make up your mind. LOL

--- src/main/java/com/worldcretornica/plotme_core/api/IEntity.java
+++ src/main/java/com/worldcretornica/plotme_core/api/IEntity.java
@@ -39,4 +39,5 @@ public interface IEntity extends IActor {
     @Override
     UUID getUniqueId();
 
+    String getName();
 }
--- src/main/java/com/worldcretornica/plotme_core/api/IOfflinePlayer.java
+++ src/main/java/com/worldcretornica/plotme_core/api/IOfflinePlayer.java
@@ -11,7 +11,6 @@ public interface IOfflinePlayer extends IActor {
      */
     boolean isOnline();
 
-
     /**
      * Returns the player name
      *
