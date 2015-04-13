c1c2c1e271cc03ab6a91697094a4e611490536b9
Matt <mattbdev@outlook.com>
2015-04-13 08:05:57 -0400

    Biome Protection

--- src/bukkit/java/com/worldcretornica/plotme_core/bukkit/PlotMeWorldEdit.java
+++ src/bukkit/java/com/worldcretornica/plotme_core/bukkit/PlotMeWorldEdit.java
@@ -1,12 +1,14 @@
 package com.worldcretornica.plotme_core.bukkit;
 
 import com.sk89q.worldedit.Vector;
+import com.sk89q.worldedit.Vector2D;
 import com.sk89q.worldedit.WorldEditException;
 import com.sk89q.worldedit.blocks.BaseBlock;
 import com.sk89q.worldedit.bukkit.BukkitPlayer;
 import com.sk89q.worldedit.extension.platform.Actor;
 import com.sk89q.worldedit.extent.AbstractDelegateExtent;
 import com.sk89q.worldedit.extent.Extent;
+import com.sk89q.worldedit.world.biome.BaseBiome;
 import com.worldcretornica.plotme_core.Plot;
 import com.worldcretornica.plotme_core.PlotId;
 import com.worldcretornica.plotme_core.PlotMeCoreManager;
@@ -19,18 +21,20 @@ public class PlotMeWorldEdit extends AbstractDelegateExtent {
     private final Extent extent;
     private final Actor actor;
     private final PlotMeCoreManager manager = PlotMeCoreManager.getInstance();
+    private final IPlayer player;
 
     public PlotMeWorldEdit(PlotMe_CorePlugin plugin, Extent extent, Actor actor) {
         super(extent);
         this.plugin = plugin;
         this.extent = extent;
         this.actor = actor;
+        player = plugin.wrapPlayer(((BukkitPlayer) actor).getPlayer());
+
 
     }
 
     @Override
     public boolean setBlock(Vector location, BaseBlock block) throws WorldEditException {
-        IPlayer player = plugin.wrapPlayer(((BukkitPlayer) actor).getPlayer());
         if (manager.isPlayerIgnoringWELimit(player)) {
             return extent.setBlock(location, block);
         } else {
@@ -40,6 +44,26 @@ public class PlotMeWorldEdit extends AbstractDelegateExtent {
                 Plot plot = manager.getPlotById(id);
                 if (plot != null && plot.isAllowed(actor.getUniqueId())) {
                     return extent.setBlock(location, block);
+                } else {
+                    return false;
+                }
+            } else {
+                return false;
+            }
+        }
+    }
+
+    @Override
+    public boolean setBiome(Vector2D position, BaseBiome biome) {
+        if (manager.isPlayerIgnoringWELimit(player)) {
+            return extent.setBiome(position, biome);
+        } else {
+            ILocation loc = new ILocation(player.getWorld(), position.getX(), 0, position.getZ());
+            PlotId id = manager.getPlotId(loc);
+            if (id != null) {
+                Plot plot = manager.getPlotById(id);
+                if (plot != null && plot.isAllowed(actor.getUniqueId())) {
+                    return extent.setBiome(position, biome);
                 } else {
                     return false;
                 }
