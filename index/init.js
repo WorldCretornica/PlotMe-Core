be6ebb6e78455f5b9ba0826f2cd3a5acc1010501
Matt <mattbdev@outlook.com>
2015-05-11 08:23:24 -0400

    Performance improvements

--- index/init.js
+++ index/init.js
@@ -12,11 +12,11 @@
 	});
 			window.onload = function() {
 				document.body.className = '';
-			}
+			};
 
 			window.ontouchmove = function() {
 				return false;
-			}
+			};
 
 			window.onorientationchange = function() {
 				document.body.scrollTop = 0;
