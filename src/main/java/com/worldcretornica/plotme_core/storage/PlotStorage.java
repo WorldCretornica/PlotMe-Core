72e7f553db40d2d5411901e9067bf7fa6e8e0e8c
Matt <mattbdev@outlook.com>
2015-05-24 15:28:21 -0400

    New PlotStorage class for API

---
+++ src/main/java/com/worldcretornica/plotme_core/storage/PlotStorage.java
@@ -0,0 +1,9 @@
+package com.worldcretornica.plotme_core.storage;
+
+/**
+ * Allows for easy synchronization between database
+ * and cache when storing Plot and User data.
+ */
+public class PlotStorage {
+
+}
