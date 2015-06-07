e564991cee5e94ef09e68f65d0c3160845eb5758
Matt <mattbdev@outlook.com>
2015-06-06 20:47:45 -0400

    Fixed #167

--- src/main/java/com/worldcretornica/plotme_core/commands/CmdAuto.java
+++ src/main/java/com/worldcretornica/plotme_core/commands/CmdAuto.java
@@ -7,8 +7,9 @@ import com.worldcretornica.plotme_core.PlotMe_Core;
 import com.worldcretornica.plotme_core.api.ICommandSender;
 import com.worldcretornica.plotme_core.api.IPlayer;
 import com.worldcretornica.plotme_core.api.IWorld;
-import com.worldcretornica.plotme_core.api.event.PlotCreateEvent;
 import net.milkbowl.vault.economy.EconomyResponse;
+
+import java.util.UUID;
 
 public class CmdAuto extends PlotCommand {
 
@@ -21,10 +22,10 @@ public class CmdAuto extends PlotCommand {
     }
 
     public boolean execute(ICommandSender sender, String[] args) throws Exception{
-        IPlayer player = (IPlayer) sender;
+        final IPlayer player = (IPlayer) sender;
         if (player.hasPermission(PermissionNames.USER_AUTO)) {
             if (manager.isPlotWorld(player) || plugin.getConfig().getBoolean("allowWorldTeleport")) {
-                IWorld world;
+                final IWorld world;
                 if (!manager.isPlotWorld(player) && plugin.getConfig().getBoolean("allowWorldTeleport")) {
                     if (args.length == 2) {
                         world = manager.getWorld(args[1]);
@@ -52,74 +53,46 @@ public class CmdAuto extends PlotCommand {
                             + " /plotme home " + C("MsgToGetToIt"));
                     return true;
                 }
-                PlotMapInfo pmi = manager.getMap(world);
-                int limit = pmi.getPlotAutoLimit();
+                final PlotMapInfo pmi = manager.getMap(world);
+                serverBridge.runTaskAsynchronously(new Runnable() {
+                    @Override public void run() {
+                        for (int i = 0; i < 50000; i++) {
+                            for (int x = -i; x <= i; x++) {
+                                for (int z = -i; z <= i; z++) {
+                                    PlotId id = new PlotId(x, z);
 
-                int x = 0;
-                int z = 0;
-                int dx = 0;
-                int dz = -1;
-                int t = limit;
-                int maxPlots = t * t;
+                                    if (manager.isPlotAvailable(id, world)) {
+                                        String name = player.getName();
+                                        UUID uuid = player.getUniqueId();
 
-                for (int i = 0; i < maxPlots; i++) {
-                    if (-limit / 2 <= x && x <= limit / 2 && -limit / 2 <= z && z <= limit / 2) {
-                        PlotId id = new PlotId(x, z);
-                        if (manager.isPlotAvailable(id, world)) {
-                            double price = 0.0;
 
-                            PlotCreateEvent event = new PlotCreateEvent(world, id, player);
-                            plugin.getEventBus().post(event);
-                            if (manager.isEconomyEnabled(pmi)) {
-                                price = pmi.getClaimPrice();
+                                        if (manager.isEconomyEnabled(world)) {
+                                            double price = pmi.getClaimPrice();
 
-                                if (serverBridge.has(player, price)) {
-                                    player.sendMessage("It costs " + serverBridge.getEconomy().get().format(price) + " to use the auto command.");
-                                    return true;
-                                } else {
-                                    if (event.isCancelled()) {
-                                        return true;
-                                    }
-                                    EconomyResponse er = serverBridge.withdrawPlayer(player, price);
+                                            if (serverBridge.has(player, price)) {
+                                                EconomyResponse er = serverBridge.withdrawPlayer(player, price);
 
-                                    if (!er.transactionSuccess()) {
-                                        player.sendMessage(er.errorMessage);
-                                        serverBridge.getLogger().warning(er.errorMessage);
-                                        return true;
-                                    }
-                                }
-                            }
+                                                if (!er.transactionSuccess()) {
+                                                    player.sendMessage(er.errorMessage);
+                                                }
+                                            } else {
+                                                player.sendMessage("You do not have enough money to buy this plot");
+                                            }
+                                        }
 
-                            if (!event.isCancelled()) {
-                                manager.createPlot(id, world, player.getName(), player.getUniqueId(), pmi);
+                                        manager.createPlot(id, world, name, uuid, pmi);
 
-                                player.setLocation(manager.getPlotHome(id, player.getWorld()));
+                                        player.teleport(manager.getPlotHome(id, world));
 
-                                player.sendMessage(C("MsgThisPlotYours") + " " + C("WordUse") + " /plotme home " + C("MsgToGetToIt"));
-
-                                if (isAdvancedLogging()) {
-                                    if (price == 0) {
-                                        serverBridge.getLogger().info(player.getName() + " " + C("MsgClaimedPlot") + " " + id);
-                                    } else {
-                                        serverBridge.getLogger()
-                                                .info(player.getName() + " " + C("MsgClaimedPlot") + " " + id + (" " + C("WordFor") + " "
-                                                        + price));
+                                        player.sendMessage(C("MsgThisPlotYours") + " " + C("WordUse") + " /plotme " + C("CommandHome") + " " + C
+                                                ("MsgToGetToIt"));
                                     }
                                 }
-                                return true;
                             }
                         }
                     }
-                    if (x == z || x < 0 && x == -z || x > 0 && x == 1 - z) {
-                        t = dx;
-                        dx = -dz;
-                        dz = t;
-                    }
-                    x += dx;
-                    z += dz;
-                }
+                });
                 player.sendMessage(C("MsgNoPlotFound"));
-                return true;
             } else {
                 player.sendMessage(C("MsgNotPlotWorld"));
             }
