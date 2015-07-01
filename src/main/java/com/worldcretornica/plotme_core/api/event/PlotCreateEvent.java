4c9b3fa3a27e282f92b3a5c1dd8b6cfb98845260
Matt <mattbdev@outlook.com>
2015-07-01 06:58:24 -0400

    Fix PlotCreateEvent

--- src/main/java/com/worldcretornica/plotme_core/api/event/PlotCreateEvent.java
+++ src/main/java/com/worldcretornica/plotme_core/api/event/PlotCreateEvent.java
@@ -3,14 +3,13 @@ package com.worldcretornica.plotme_core.api.event;
 import com.worldcretornica.plotme_core.PlotId;
 import com.worldcretornica.plotme_core.api.IPlayer;
 
-public class PlotCreateEvent extends PlotEvent implements ICancellable, Event {
+public class PlotCreateEvent implements ICancellable, Event {
 
     private final PlotId plotId;
     private final IPlayer creator;
     private boolean canceled;
 
     public PlotCreateEvent(PlotId plotId, IPlayer creator) {
-        super(null);
         this.plotId = plotId;
         this.creator = creator;
     }
